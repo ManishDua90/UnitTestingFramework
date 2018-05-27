bd79f789d866ed085ef8593e3326eb72d176a420
MANISHDUA90 <manish.dua.itengg@gmail.com>
2018-05-27 17:18:23 +0530

    Batch file for jenkins

--- testNGBatchFile.bat
+++ testNGBatchFile.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\Users\ManishDua\IdeaProjects\junittestNGexample
+set projectLocation=C:\Users\***\IdeaProjects\junittestNGexample
 cd %projectLocation%
 set classpath=C:\Maven\apache-maven-3.5.2\mvnRepo\org\testng\testng\6.11\testng-6.11.jar;%projectLocation%\target\test-classes;C:\Maven\apache-maven-3.5.2\mvnRepo\com\beust\jcommander\1.64\jcommander-1.64.jar;
 java org.testng.TestNG testng.xml
